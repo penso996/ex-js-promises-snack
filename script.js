dd025c0dfa7848b8e669f933a100bd31f9daeeee
penso996 <penso.andrea@outlook.it>
2025-05-19 13:28:13 +0200

    snack 2

--- script.js
+++ script.js
@@ -6,40 +6,58 @@
 // Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore,
 // recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
 
-function getPostTitle(id) {
-    return new Promise((resolve, reject) => {
-        fetch(`https://dummyjson.com/posts/${id}`)
-            .then(res => res.json())
-            .then(post => resolve(post.title))
-            .catch(reject)
-    });
-}
+// function getPostTitle(id) {
+//     return new Promise((resolve, reject) => {
+//         fetch(`https://dummyjson.com/posts/${id}`)
+//             .then(res => res.json())
+//             .then(post => resolve(post.title))
+//             .catch(reject)
+//     });
+// }
 
-getPostTitle(1)
-    .then(title => console.log("Questo è il titolo:", title))
-    .catch(error => console.error(error));
+// getPostTitle(1)
+//     .then(title => console.log("Questo è il titolo:", title))
+//     .catch(error => console.error(error));
 
-//BONUS
-function getPost(id) {
-    return new Promise((resolve, reject) => {
-        fetch(`https://dummyjson.com/posts/${id}`)
-            .then(res => res.json())
-            .then(post => {
-                fetch(`https://dummyjson.com/users/${post.userId}`)
-                    .then(res => res.json())
-                    .then(user => resolve({ ...post, user }))
-                    .catch(reject)
-            })
-            .catch(reject)
-    });
-}
+// //BONUS
+// function getPost(id) {
+//     return new Promise((resolve, reject) => {
+//         fetch(`https://dummyjson.com/posts/${id}`)
+//             .then(res => res.json())
+//             .then(post => {
+//                 fetch(`https://dummyjson.com/users/${post.userId}`)
+//                     .then(res => res.json())
+//                     .then(user => resolve({ ...post, user }))
+//                     .catch(reject)
+//             })
+//             .catch(reject)
+//     });
+// }
 
-getPost(1)
-    .then(title => console.log("Questo è il post completo:", title))
-    .catch(error => console.error(error));
+// getPost(1)
+//     .then(title => console.log("Questo è il post completo:", title))
+//     .catch(error => console.error(error));
 
 // Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6.
 // Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
 // Bonus: HOF con closure per memorizzare l'ultimo lancio
 // Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato.
 // Se il numero esce due volte di fila, stampa "Incredibile!".
+
+const lanciaDado = () => {
+    return new Promise((resolve, reject) => {
+        console.log("Sto lanciando il dado...");
+        setTimeout(() => {
+            if (Math.random() < 0.2) {
+                reject("Il dado si è incastrato")
+            } else {
+                const risultato = Math.floor(Math.random() * 6) + 1;
+                resolve(risultato);
+            }
+        }, 3000)
+    })
+}
+
+lanciaDado()
+    .then(risultato => console.log("Il risultato è", risultato))
+    .catch(err => console.error(err));
